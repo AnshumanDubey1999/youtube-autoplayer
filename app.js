f77187bd430775391ec10a2050af7dfcd4e154d4
Aditya <adityaagupta31@gmail.com>
2021-04-13 20:56:23 +0530

    small bug fixed

--- app.js
+++ app.js
@@ -3,47 +3,50 @@ const fs = require('fs')
 let app = express()
 
 require("dotenv").config()
+/**
+ * ! Deprecated
+ */
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 
 
 let obj = {
   title: 'Video title how to autoplay YouTube video with YouTube url',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto temporibus libero voluptatibus aut excepturi dolores perferendis asperiores eum. Dolorem, ab ad! Assumenda eius dolor quasi neque laborum animi sit totam itaque dolorum, obcaecati ullam libero rerum ratione saepe autem quae, repellendus debitis, consectetur doloremque eveniet exercitationem quam dignissimos accusamus excepturi?',
   videoID: 'Xc25RykxUrM',
-  otherVideos: [ 'Xc25RykxUrM','Xc25RykxUrM','Xc25RykxUrM' ]
+  otherVideos: ['Xc25RykxUrM', 'Xc25RykxUrM', 'Xc25RykxUrM']
 }
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   let data = fs.readFileSync('./public/data.json')
   // console.log(data)
-  res.render('index', {data: JSON.parse(data)})
+  res.render('index', { data: JSON.parse(data) })
 })
 
-app.get('/admin-panel', function(req, res){
+app.get('/admin-panel', function (req, res) {
   let data = fs.readFileSync('./public/data.json')
   let obj = JSON.parse(data)
   obj.ids = obj.otherVideos.join('|')
-  res.render('edit', {data: obj})
+  res.render('edit', { data: obj })
 })
 
-app.post('/admin-panel', function(req, res){
+app.post('/admin-panel', function (req, res) {
   // console.log(req.body)
   // console.log(req.body.key, process.env.KEY)
-  if(req.body.key != process.env.KEY){
+  if (req.body.key != process.env.KEY) {
     console.log('Incorrect Password')
     res.redirect('/admin-panel')
   }
-  else{
+  else {
     let data = req.body.val
     data.otherVideos = data.ids.split('|')
-    fs.writeFileSync('./public/data.json', JSON.stringify(data), function(err){
-      if(err)
+    fs.writeFileSync('./public/data.json', JSON.stringify(data), function (err) {
+      if (err)
         console.log("Error Occured!")
     })
     res.redirect('/')
@@ -61,10 +64,10 @@ var rdata =
 console.log(JSON.parse(rdata))
 */
 
-if(process.env.PORT)
+if (process.env.PORT)
   app.listen(process.env.PORT, process.env.IP)
-else{
-  app.listen(3000, function(){
+else {
+  app.listen(3000, function () {
     console.log("Autoplayer is on FIRE!!!")
   })
 }
